4df8ad6e9494033a86506512badd89ee0cbb2706
Pedro Gomes <pedroleinar@hotmail.com>
2020-05-24 00:25:52 -0300

    AppError

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (r, res) => res.json({ message: 'Hello' }));
+routes.get('/', (_, res) => res.json({ message: 'Hello' }));
 
 export default routes;
